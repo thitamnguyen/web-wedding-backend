9a41bf17b05d345aeab09c1d5da5b5f2279e7482
thitamnguyen <tamn7161@gmail.com>
2026-06-13 10:28:03 +0700

    fix

--- src/main/java/com/example/demo/repository/BookingRepository.java
+++ src/main/java/com/example/demo/repository/BookingRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Booking;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
